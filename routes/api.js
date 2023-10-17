a07dd5bc622762e188f4e78099dae50f5aca0045
Serg Skol <sps676@gmail.com>
2023-10-17 13:30:54 +0200

    Add API route

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,29 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+  app.route("/api/convert").get( (req, res) => {
+    const input = req.query.input;
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
 
+    if ( !initNum && !initUnit ) {
+      return res.send( 'invalid number and unit' );
+    } else if ( !initNum ) {
+      return res.send( 'invalid number' );
+    } else if ( !initUnit ) {
+      return res.send( 'invalid unit' );
+    };
+
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+    
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string,
+    });
+  });
 };
